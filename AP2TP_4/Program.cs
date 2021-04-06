e57517ba2868f05197e64f9675dbf68d49c045f9
Petr Zámečník <petrz.zamecnik@seznam.cz>
2021-04-07 00:01:00 +0200

    added input string

--- AP2TP_4/Program.cs
+++ AP2TP_4/Program.cs
@@ -9,35 +9,41 @@ namespace AP2TP_4
     {
         public static void Main(string[] args)
         {
+            // input string
             var input = "sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras " +
-                           "semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequa" +
-                           "t id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus" +
-                           " ut faucibus pulvinar elementum integer enim neque volutpat ac";
+                             "semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequa";
+                           
 
             input = input.ToUpper();
+
+            // variables
+            var letters = new Dictionary<char, int>();
+            var letterCount = input.Length;
+            var probability = 0;
+
             
-            var letters = new Dictionary<string, int>()
+            // check if character c is in letter, if not add it, if yes, add value +1
+            foreach (var c in input)
             {
-                {"A", 0}, {"B", 0}, {"C", 0}, {"D", 0}, {"E", 0},
-                {"F", 0}, {"G", 0}, {"H", 0}, {"I", 0}, {"J", 0}, 
-                {"K", 0}, {"L", 0}, {"M", 0}, {"N", 0}, {"O", 0},
-                {"P", 0}, {"Q", 0}, {"R", 0}, {"S", 0}, {"T", 0},
-                {"U", 0}, {"V", 0}, {"W", 0}, {"X", 0}, {"Y", 0},
-                {"Z", 0}
-            };
-
-            var a = "A";
-
-
-            var charArray = new char[input.Length];
-            charArray = input.ToCharArray();
-            foreach (var c in charArray)
-            {
-                if (letters.ContainsKey(c.ToString()))
+                if (letters.ContainsKey(c))
                 {
-                    
+                    letters[c]++;
+                }
+                else
+                {
+                    letters[c] = 1;
                 }
             }
+
+            Console.WriteLine($"Pocet znaku: {letterCount}");
+
+            foreach (var c in letters)
+            {
+               
+            }
+            
+
+            
         }
 
         private static string ShannonFanova(string input)
